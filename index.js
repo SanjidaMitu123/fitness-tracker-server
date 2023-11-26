16b356c65de3b34fe68705ff72b5fcabbf26ba96
SanjidaMitu123 <sanjidamitu456@gmail.com>
2023-11-26 16:18:38 +0600

    data ID wise  get  from  class  table

--- index.js
+++ index.js
@@ -55,6 +55,13 @@ async function run() {
       res.send(result);
     })
 
+    app.get('/classes/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await  classCollection.findOne(query);
+      res.send(result);
+    })
+
     app.get('/teacher', async(req,res)=>{
       const cursor = teacherCollection.find();
       const result = await cursor.toArray();
